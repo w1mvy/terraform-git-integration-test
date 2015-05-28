33a58083ee983027691d8d96f623faaa35dc6311
w1mvy <w1mvypine@gmail.com>
2015-05-28 16:40:08 +0900

    Add ssh config

--- main.tf
+++ main.tf
@@ -76,3 +76,21 @@ resource "aws_security_group" "production" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "ssh" {
+  name = "ssh"
+  description = "Allow all ssh traffic"
+  vpc_id = "${var.vpc_id}"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    self        = true
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
